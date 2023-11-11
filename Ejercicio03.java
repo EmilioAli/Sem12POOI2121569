e0634aff2d4b15ea120bda06e238f3da04fef68e
EmilioAli <143912439+EmilioAli@users.noreply.github.com>
2023-11-11 11:23:00 -0500

    Update Ejercicio03.java

--- Ejercicio03.java
+++ Ejercicio03.java
@@ -16,3 +16,26 @@ public class Main{
         }
     }
 }
+
+import java.util.Scanner;
+
+public class Empleado {
+    private String nombre;
+    private int numeroEmpleado;
+
+    public void leerDatos() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el nombre del empleado:");
+        this.nombre = scanner.nextLine();
+
+        System.out.println("Ingrese el número de empleado:");
+        this.numeroEmpleado = scanner.nextInt();
+    }
+
+    public void verDatos() {
+        System.out.println("Datos del empleado:");
+        System.out.println("Nombre: " + this.nombre);
+        System.out.println("Número de empleado: " + this.numeroEmpleado);
+    }
+}
